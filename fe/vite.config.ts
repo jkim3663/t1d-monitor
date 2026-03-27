14815c351b945b6a72c37047027ea872d1e915cc
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2026-03-27 08:26:50 -0400

    separate local and stage config and dockerfile for stage

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.WEB_SERVER_PORT) || 5173,
+    },
+    preview: {
+      port: Number(env.WEB_SERVER_PORT) || 5173,
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      __APP_SERVER_URL__: JSON.stringify(env.APP_SERVER_URL),
+    },
+  }
 })
