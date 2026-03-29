9dd6ee8b97ba609f63fe25b468228201aa6503a8
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2026-03-28 21:46:15 -0400

    add spring template for backend

---
+++ be/t1d/src/main/java/be/t1d/T1dApplication.java
@@ -0,0 +1,13 @@
+package be.t1d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1dApplication.class, args);
+	}
+
+}
---
+++ be/t1d/src/test/java/be/t1d/T1dApplicationTests.java
@@ -0,0 +1,13 @@
+package be.t1d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
