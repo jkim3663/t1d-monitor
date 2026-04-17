1ad8f1c53e5bb771c30b6728f3ad78f37c86aa21
Jun Yeop Kim <84053682+jkim3663@users.noreply.github.com>
2026-04-16 22:34:39 -0400

    go app and db connection established

---
+++ be/internal/db/conn.go
@@ -0,0 +1,53 @@
+package dbconnection
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool() (*pgxpool.Pool, error) {
+	host := os.Getenv("AUTH_DB_HOST")
+	port := os.Getenv("AUTH_DB_PORT")
+	user := os.Getenv("AUTH_DB_USER")
+	password := os.Getenv("AUTH_DB_PASSWORD")
+	dbName := os.Getenv("AUHT_DB_NAME")
+	sslmode := os.Getenv("AUTH_DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		user,
+		password,
+		host,
+		port,
+		dbName,
+		sslmode,
+	)
+	var dbPool *pgxpool.Pool
+	var err error
+	for i := 0; i < 15; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		dbPool, err = pgxpool.New(ctx, dsn)
+		if err == nil {
+			err = dbPool.Ping(ctx)
+		}
+		cancel()
+		if err == nil {
+			log.Println("database pool successfully created")
+			return dbPool, nil
+		}
+		if dbPool != nil {
+			dbPool.Close()
+		}
+		time.Sleep(2 * time.Second)
+
+	}
+	return dbPool, nil
+}
--- be/main.go
+++ be/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"jkim3663/applogin/handlers"
+	dbconnection "jkim3663/applogin/internal/db"
 	"jkim3663/applogin/internal/initializer"
+	"jkim3663/applogin/internal/sql"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +14,14 @@ import (
 
 func main() {
 	initializer.LoadEnvVariables()
+	pool, err := dbconnection.NewPool()
+	if err != nil {
+		log.Fatalf("db connection failed: %v", err)
+	}
+	defer pool.Close()
+
+	queries := sql.New(pool)
+	_ = queries
 
 	router := mux.NewRouter()
 	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
@@ -19,8 +29,7 @@ func main() {
 	log.Println("Starting the server")
 
 	serverUrl := os.Getenv("GO_APP_URL") + ":" + os.Getenv("PORT")
-	err := http.ListenAndServe(serverUrl, router)
-	if err != nil {
+	if err := http.ListenAndServe(serverUrl, router); err != nil {
 		log.Println("Could not start the server", err)
 	}
 }
